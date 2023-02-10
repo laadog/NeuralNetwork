1a56ddbe9618b31c9e73d2628dad44a8ea6ab1f7
laadog <101477284+laadog@users.noreply.github.com>
2023-02-10 19:57:06 +0200

    GPU trainer

--- gpu.js
+++ gpu.js
@@ -30,29 +30,67 @@ function multiply(a, b){
 }
 
 
-let a = [[1,2,3]]
-let b = [[1,2, 3], [3,4, 3], [5, 6, 3]]
+// let a = [1, 2]
+// let b = 
+// [
+//     [ 
+//         [[ 8, -8 ], [ -5, 4 ] ],
+//         [ [ 8, -3 ], [ 6, 3 ] ],
+//         [ [ -3 ], [ 4 ] ]
+//     ],
+//     [ 
+//         [[ 8, -8 ], [ -5, 4 ] ],
+//         [ [ 8, -3 ], [ 6, 3 ] ],
+//         [ [ -3 ], [ 4 ] ]
+//     ]
+// ]
+
+
+let a = [
+    [
+        [1,2],
+        [4,5]
+    ],
+    [
+        [1,2],
+        [4,5]
+    ]
+]
+let b = [
+    [
+        [5,4],
+        [6,8],
+    ],
+    [
+        [3,4],
+        [6,8],
+    ],
+]
+
+
+/*
+
+[
+    [[], []],
+    [[], []],
+]
+
+*/
 
 const gpu = new GPU();
 const multiplyMatrix = gpu.createKernel(function(a, b, length) {
 let sum = 0;
 for (let i = 0; i < length; i++) {
-    sum += a[this.thread.y][i] * b[i][this.thread.x];
+    sum += a[this.thread.z][this.thread.y][i] * b[this.thread.z][i][this.thread.x];
 }
 return sum;
-}).setOutput([3, 1])
+}).setOutput([2, 2, 2]) // output length, inputs count,  networks count
 
-let gpuOut = multiplyMatrix(a, b, b.length)
+
+let gpuOut = multiplyMatrix(a, b, 2)
 
 console.log(gpuOut)
-console.log(multiply(a, b))
 
-// console.time("Cpu")
-// const cpuOut = multiply(matrices[0], matrices[1])
-// console.timeEnd("Cpu")
-// console.time("Gpu")
-// gpuOut = multiplyMatrix(matrices[0], matrices[1], SIZE)
-// console.timeEnd("Gpu")
 
 
 
--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const { GPU } = require('gpu.js');
 
-class Network{
+class Network {
     inputs;
     outputs;
     layers = [];
 
-    constructor(inputs, outputs){
+    constructor(inputs, outputs) {
         this.inputs = inputs;
         this.outputs = outputs;
     }
 
-    generate(depths){ 
-        for(var i = 0; i < depths.length + 1; i++){
+    generate(depths) {
+        for (var i = 0; i < depths.length + 1; i++) {
             this.layers[i] = []
-            for(var j = 0; j < [this.inputs, ...depths, this.outputs][i]; j++){
+            for (var j = 0; j < [this.inputs, ...depths, this.outputs][i]; j++) {
                 this.layers[i][j] = []
-                for(var z = 0; z < [this.inputs, ...depths, this.outputs][i+1];z++){
-                    this.layers[i][j][z] = Math.floor((1 - 2*Math.random())*10)
+                for (var z = 0; z < [this.inputs, ...depths, this.outputs][i + 1]; z++) {
+                    this.layers[i][j][z] = Math.floor((1 - 2 * Math.random()) * 10)
                 }
             }
         }
 
     }
 
-    calculate(input){
+    calculate(input) {
         var nextLayer = [];
-        for(var i = 0; i <this.layers.length;i++){
-            for(var j = 0; j < this.layers[i][0].length;j++){
+        for (var i = 0; i < this.layers.length; i++) {
+            for (var j = 0; j < this.layers[i][0].length; j++) {
                 nextLayer[j] = 0;
-                for(var z = 0; z < input.length; z++){
-                    nextLayer[j] += input[z]*this.layers[i][z][j]
+                for (var z = 0; z < input.length; z++) {
+                    nextLayer[j] += input[z] * this.layers[i][z][j]
                 }
             }
             input = []
-            for(let j = 0; j < nextLayer.length; j++){
+            for (let j = 0; j < nextLayer.length; j++) {
                 input[j] = nextLayer[j]
             }
             nextLayer = []
@@ -41,10 +41,10 @@ class Network{
         return input;
     }
 
-    calculateGPU(input){
+    calculateGPU(input) {
         const gpu = new GPU();
-        for(let i = 0; i < this.layers.length; i++){
-            const multiplyMatrix = gpu.createKernel(function(a, b, size) {
+        for (let i = 0; i < this.layers.length; i++) {
+            const multiplyMatrix = gpu.createKernel(function (a, b, size) {
                 let sum = 0;
                 for (let i = 0; i < size; i++) {
                     sum += a[this.thread.y][i] * b[i][this.thread.x];
@@ -56,23 +56,23 @@ class Network{
         return input;
     }
 
-    mutate(percentage = 0.8, offset = 0.1){
+    mutate(percentage = 0.8, offset = 0.1) {
         var mutated = new Network(this.inputs, this.outputs)
 
-        for(var i = 0; i < this.layers.length; i++){
-            
+        for (var i = 0; i < this.layers.length; i++) {
+
             mutated.layers[i] = []
             for (var j = 0; j < this.layers[i].length; j++) {
                 mutated.layers[i][j] = []
-                for(var z = 0; z < this.layers[i][j].length; z++){
-                    if(Math.random() < percentage){
-                        mutated.layers[i][j][z] = this.layers[i][j][z] + (1 - 2*Math.random())*offset
+                for (var z = 0; z < this.layers[i][j].length; z++) {
+                    if (Math.random() < percentage) {
+                        mutated.layers[i][j][z] = this.layers[i][j][z] + (1 - 2 * Math.random()) * offset
 
-                    }else{
+                    } else {
                         mutated.layers[i][j][z] = this.layers[i][j][z]
                     }
                 }
-                
+
             }
         }
         return mutated;
@@ -80,46 +80,123 @@ class Network{
 
 
 
-    get getLayers(){
+    get getLayers() {
         return this.layers
     }
 }
 
-function train(base, layers, count,checks , generations, mutation ,answer){
-    var networks = []
-    for(var i = 0; i < count;i++){
-        networks[i] = new Network(2,1)
-        networks[i].generate(layers)
-    }
-    
-    for(var i = 0; i < generations; i++){
-        let closest, smallestOffset;
-        for(var j = 0; j < networks.length;j++){
-            var offset = 0;
-            for(var z = 0; z < checks; z++){
-                let prompt = answer();
-                let a = networks[j].calculate(prompt.input)[0]
-                offset += Math.abs(a-prompt.output)
+class Trainer {
+    train(inputs, outputs, layers, count, checks, generations, mutation, answer) {
+        var networks = []
+        for (var i = 0; i < count; i++) {
+            networks[i] = new Network(inputs, outputs)
+            networks[i].generate(layers)
+        }
+
+        for (var i = 0; i < generations; i++) {
+            let closest, smallestOffset;
+            for (var j = 0; j < networks.length; j++) {
+                var offset = 0;
+                for (var z = 0; z < checks; z++) {
+                    let prompt = answer();
+                    let a = networks[j].calculate(prompt.input)[0]
+                    offset += Math.abs(a - prompt.output)
+                }
+                offset /= checks;
+                if (!smallestOffset || offset < smallestOffset) {
+                    smallestOffset = offset
+                    closest = j;
+                }
             }
-            offset /= checks;
-            if(!closest || offset < smallestOffset){
-                smallestOffset = offset
-                closest = j;
+            console.log(`Gen: ${i}, offset: ${smallestOffset}`)
+            networks[0] = networks[closest];
+            for (var j = 1; j < networks.length; j++) {
+                networks[j] = networks[closest].mutate(...mutation);
             }
         }
-        console.log(`Gen: ${i}, offset: ${smallestOffset}`)
-        networks[0] = networks[closest];
-        for(var j = 1; j < networks.length; j++){
-            networks[j] = networks[closest].mutate(...mutation);
-        }
+        return networks[0]
+
     }
-    return networks[0]
+
+    trainGPU(inputs, outputs, layers, count, checks, generations, mutation, answer) {
+        var networks = [];
+
+        for (let i = 0; i < count; i++) {
+            let t = new Network(inputs, outputs)
+            t.generate(layers)
+            networks[i] = t.layers;
+        }
+
+        const gpu = new GPU();
+
+        const multiply = gpu.createKernel(function (a, b, length) {
+            let sum = 0;
+            for (let i = 0; i < length; i++) {
+                sum += a[this.thread.z][this.thread.y][i] * b[this.thread.z][i][this.thread.x];
+            }
+            return sum;
+        })
+
+        multiply.setDynamicOutput(true)
+
+        for (let i = 0; i < generations; i++) {
+            let input = []
+            let output = []
+            for (let j = 0; j < networks.length; j++) {
+                input[j] = []
+                output[j] = []
+                for (let z = 0; z < checks; z++) {
+                    let t = answer()
+                    input[j][z] = t.input;
+                    output[j][z] = t.output;
+                }
+            }
+
+            let n = new Network(inputs, outputs)
+            n.layers = networks[0]
+
+            for (let j = 0; j < networks[0].length; j++) {
+                multiply.setOutput([networks[0][j][0].length, checks, count])
+                input = multiply(input, networks, networks[0][j].length);
+            }
+
+            let closest, smallestOffset;
+            for (let j = 0; j < networks.length; j++) {
+                let offset = 0;
+                for (let z = 0; z < checks; z++) {
+                    offset += Math.abs(output[j][z] - input[j][z][0])
+                }
+                offset /= checks
+                if (!smallestOffset || offset < smallestOffset) {
+                    smallestOffset = offset
+                    closest = j;
+                }
+            }
+
+            console.log(`Gen: ${i}, offset: ${smallestOffset}`)
+            networks[0] = networks[closest]
+            let t = new Network(inputs, outputs);
+            t.layers = networks[0]
+            for (let j = 1; j < networks.length; j++) {
+                networks[j] = t.mutate(...mutation).layers
+            }
+        }
+
+        let t = new Network(inputs, outputs);
+        t.layers = networks[0]
+        return t;
+
+    }
 
 }
 
+var trainer = new Trainer();
 
-let n = new Network(2, 1)
+trainer.trainGPU(2, 1, [], 10, 10, 1000, [0.5, 0.1], () => {
+    let x = Math.floor(Math.random() * 10)
+    let y = Math.floor(Math.random() * 10)
+    let output = x + y
+    return { output, input: [x, y] }
+})
 
-n.generate([2, 2, 2])
-console.log(n.calculateGPU([1, 2]))
-console.log(n.calculate([1, 2]))
+
